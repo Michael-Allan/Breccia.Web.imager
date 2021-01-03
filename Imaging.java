f1285d0e5a29305344aec6c3965fb225e7286937
Michael Allan <mike@reluk.ca>
2021-01-03 11:54:00 -0500

    Reuseable code for image commands.

---
+++ Imaging.java
@@ -0,0 +1,60 @@
+package Breccia.Web.imager;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import Java.Unhandled;
+import Java.UserError;
+
+import static Java.Files.verifyDirectoryArgument;
+import static java.nio.file.Files.walkFileTree;
+import static java.nio.file.FileVisitResult.CONTINUE;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+
+public final class Imaging {
+
+
+    private Imaging() {}
+
+
+
+    /** Executes an `image` shell command.
+      *
+      *     @param mould The image mould to use.
+      *     @return True on success, false on failure.
+      *     @see <a href='http://reluk.ca/project/Breccia/Web/imager/bin/image.brec'>
+      *       The `image` command of the Breccia Web imager</a>
+      *     @see <a href='http://reluk.ca/project/wayic/Web/imager/bin/image.brec'>
+      *       The `image` command of the waycast Web imager</a>
+      */
+    public static boolean image( final ImageMould mould ) {
+        boolean hasFailed = false;
+        try { mould.formImage(); }
+        catch( final UserError x ) {
+            System.err.println( "image: " + x.getMessage() );
+            hasFailed = true; }
+        try { placeImageFiles( /*from*/mould.outDirectory, /*to*/mould.boundaryPathDirectory ); }
+        catch( IOException x ) { throw new Unhandled( x ); } /* Failure might occur owing to an
+          unwritable directory, but this is unlikely; the mould images only writeable directories. */
+        return !hasFailed; }
+
+
+
+    /** Moves all simple files of directory `dFrom` to the same relative path of `dTo`,
+      * replacing any that are already present.
+      *
+      *     @throws IllegalArgumentException Unless `dFrom` and `dTo` are directories.
+      */
+    public static void placeImageFiles​( final Path dFrom, final Path dTo ) throws IOException {
+        verifyDirectoryArgument( dFrom );
+        verifyDirectoryArgument( dTo );
+        walkFileTree( dFrom, new SimpleFileVisitor<Path>() {
+            public @Override FileVisitResult visitFile( final Path f, BasicFileAttributes _a )
+                  throws IOException {
+                Files.move( f, dTo.resolve(dFrom.relativize(f)), REPLACE_EXISTING );
+                return CONTINUE; }});}}
+
+
+
+                                                   // Copyright © 2020-2021  Michael Allan.  Licence MIT.
--- Project.java
+++ Project.java
@@ -1,18 +1,10 @@
 package Breccia.Web.imager;
 
-import java.io.IOException;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Path;
 import java.util.logging.Logger;
 
-import static Java.Files.verifyDirectoryArgument;
-import static java.nio.file.Files.walkFileTree;
-import static java.nio.file.FileVisitResult.CONTINUE;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
-
-/** The present project.  Included are one or two miscellaneous resources,
-  * residual odds and ends that properly fit nowhere else.
+/** The present project.
   */
 public final class Project {
 
@@ -30,23 +22,7 @@ public final class Project {
     /** The output directory of the present project.
       */
     public static final Path outDirectory = Path.of(
-      System.getProperty("java.io.tmpdir"), "Breccia.Web.imager" );
-
-
-
-    /** Moves all simple files of directory `dFrom` to the same relative path of `dTo`,
-      * replacing any that are already present.
-      *
-      *     @throws IllegalArgumentException Unless `dFrom` and `dTo` are directories.
-      */
-    public static void placeImageFiles​( final Path dFrom, final Path dTo ) throws IOException {
-        verifyDirectoryArgument( dFrom );
-        verifyDirectoryArgument( dTo );
-        walkFileTree( dFrom, new SimpleFileVisitor<Path>() {
-            public @Override FileVisitResult visitFile( final Path f, BasicFileAttributes _a )
-                  throws IOException {
-                Files.move( f, dTo.resolve(dFrom.relativize(f)), REPLACE_EXISTING );
-                return CONTINUE; }});}}
+      System.getProperty("java.io.tmpdir"), "Breccia.Web.imager" ); }
 
 
 
