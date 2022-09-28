c5c769e5cb5e310b57afa797c1e797dd053bd86e
Michael Allan <mike@reluk.ca>
2022-09-28 08:34:49 -0400

    Redundancy removal.

--- CleaningOptions.java
+++ CleaningOptions.java
@@ -1,20 +1,5 @@
 package Breccia.Web.imager;
 
-//import java.io.IOException;
-//import java.io.PrintStream;
-//import java.nio.file.Path;
-//import Java.Unhandled;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
-
-//import static java.io.OutputStream.nullOutputStream;
-//import static java.lang.Float.parseFloat;
-//import static java.lang.Integer.parseUnsignedInt;
-//import static java.lang.System.err;
-//import static java.nio.file.Files.readString;
-//import static Java.Paths.enslash;
-//import static Java.URI_References.isRemote;
-
 
 public final class CleaningOptions extends Options {
 
